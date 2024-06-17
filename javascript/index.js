da477675863960d0e0a7abbfc93e3114973a712a
Joicemotamoreira <joicemotamoreira@live.com>
2024-06-17 11:59:59 +0100

    Added functionality to select

--- javascript/index.js
+++ javascript/index.js
@@ -22,3 +22,26 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div>
+            <div class="time">${cityTime.format(
+              "hh:mm:ss"
+            )} <small>${cityTime.format("A")}</small></div>
+          </div>
+        </div>
+  `;
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
