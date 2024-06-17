3913888295d359c4d9a105a001accb151cfea29d
Joicemotamoreira <joicemotamoreira@live.com>
2024-06-17 11:13:50 +0100

    Added libraries and basic JS

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  //dublin
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "hh:mm:ss [<small>]A[<small>]"
+  );
+
+  //paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("hh:mm:ss [<small>]A[<small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
