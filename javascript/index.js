891f529bba0301afef5ce04207474c8a634e9c45
Joicemotamoreira <joicemotamoreira@live.com>
2024-06-17 15:00:42 +0100

    Fixed update time of selected cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,43 @@
+let genericCityEventValue = null;
+
 function updateTime() {
   //dublin
   let dublinElement = document.querySelector("#dublin");
-  let dublinDateElement = dublinElement.querySelector(".date");
-  let dublinTimeElement = dublinElement.querySelector(".time");
-  let dublinTime = moment().tz("Europe/Dublin");
+  if (!!dublinElement) {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
 
-  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
-  dublinTimeElement.innerHTML = dublinTime.format(
-    "hh:mm:ss [<small>]A[<small>]"
-  );
+    dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format(
+      "hh:mm:ss [<small>]A[<small>]"
+    );
+  }
 
   //fortaleza
   let fortalezaElement = document.querySelector("#fortaleza");
-  let fortalezaDateElement = fortalezaElement.querySelector(".date");
-  let fortalezaTimeElement = fortalezaElement.querySelector(".time");
-  let fortalezaTime = moment().tz("America/Fortaleza");
+  if (!!fortalezaElement) {
+    let fortalezaDateElement = fortalezaElement.querySelector(".date");
+    let fortalezaTimeElement = fortalezaElement.querySelector(".time");
+    let fortalezaTime = moment().tz("America/Fortaleza");
 
-  fortalezaDateElement.innerHTML = fortalezaTime.format("MMMM Do YYYY");
-  fortalezaTimeElement.innerHTML = fortalezaTime.format(
-    "hh:mm:ss [<small>]A[<small>]"
-  );
+    fortalezaDateElement.innerHTML = fortalezaTime.format("MMMM Do YYYY");
+    fortalezaTimeElement.innerHTML = fortalezaTime.format(
+      "hh:mm:ss [<small>]A[<small>]"
+    );
+  }
+
+  let genericCityElement = document.querySelector("#genericCity");
+  if (!!genericCityElement) {
+    let genericCityDateElement = genericCityElement.querySelector(".date");
+    let genericCityTimeElement = genericCityElement.querySelector(".time");
+    let genericCityTime = moment().tz(genericCityEventValue);
+
+    genericCityDateElement.innerHTML = genericCityTime.format("MMMM Do YYYY");
+    genericCityTimeElement.innerHTML = genericCityTime.format(
+      "hh:mm:ss [<small>]A[<small>]"
+    );
+  }
 }
 
 updateTime();
@@ -27,6 +45,7 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  genericCityEventValue = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
@@ -34,7 +53,7 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-  <div class="city">
+  <div class="city" id="genericCity">
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
